6b94ec05b43181970b049f61b2a8d6845a889409
NEEEEEEEED <120690595+NEEEEEEEED@users.noreply.github.com>
2023-02-23 16:58:32 +0900

    2023.02.23 1차 머지

---
+++ MiddleProject/src/main/java/com/yedam/order/service/OrderServiceImpl.java
@@ -0,0 +1,29 @@
+package com.yedam.order.service;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.yedam.common.DataSource;
+import com.yedam.order.mapper.OrderMapper;
+import com.yedam.order.vo.OrderVO;
+import com.yedam.product.mapper.ProductMapper;
+import com.yedam.product.vo.ProductVO;
+
+public class OrderServiceImpl implements OrderService{
+	SqlSession session = DataSource.getInstance().openSession(true);
+	OrderMapper mapper = session.getMapper(OrderMapper.class);
+
+	@Override
+	public List<OrderVO> orderList() {
+		return mapper.getList();
+	}
+	
+	@Override
+	public OrderVO getOrder(String orderId) {
+		return mapper.getOrder(orderId);
+	}
+
+	
+	
+}
